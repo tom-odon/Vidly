45ced86a3853ef43cc87ab3a49f0505a125b74b0
tom_odon <thomas.odon@gmail.com>
2017-06-19 17:10:27 -0500

    Playing with new Routes and Actions.

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,14 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+            //Most Specific to most generic
+            routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { controller = "Movies", action = "ByReleaseDate"},
+                new { year = @"\d{4}", month = @"\d{2}" }); 
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -9,11 +9,41 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private object mvcaction4;
+
         // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
             return View(movie);
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id= " + id);
+        }
+
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy{1}", pageIndex, sortBy));
+        }
+
+            public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+        
     }
 }
